46131d703b1d8a11dce7fb9aa8be5199e2b40086
Brent Yorgey <byorgey@cis.upenn.edu>
2013-03-05 10:18:11 -0500

    switch to Custom build type + copy diagrams/ into Haddock output dir

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,27 @@
-import Distribution.Simple
-main = defaultMain
+import           Data.List                 (isSuffixOf)
+import           Distribution.Simple
+import           Distribution.Simple.Setup (Flag (..), HaddockFlags,
+                                            haddockDistPref)
+import           Distribution.Simple.Utils (copyFiles)
+import           Distribution.Text         (display)
+import           Distribution.Verbosity    (normal)
+import           System.Directory          (getDirectoryContents)
+import           System.FilePath           ((</>))
+
+-- Ugly hack, logic copied from Distribution.Simple.Haddock
+haddockOutputDir :: Package pkg => HaddockFlags -> pkg -> FilePath
+haddockOutputDir flags pkg = destDir
+   where
+     baseDir = case haddockDistPref flags of
+                      NoFlag -> "."
+                      Flag x -> x
+     destDir = baseDir </> "doc" </> "html" </> display (packageName pkg)
+
+main :: IO ()
+main = defaultMainWithHooks simpleUserHooks
+         { postHaddock = \args flags pkg lbi -> do
+             dias <- filter ("svg" `isSuffixOf`) `fmap` getDirectoryContents "diagrams"
+             copyFiles normal (haddockOutputDir flags pkg)
+               (map (\d -> ("", "diagrams" </> d)) dias)
+             postHaddock simpleUserHooks args flags pkg lbi
+         }

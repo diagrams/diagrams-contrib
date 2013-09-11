4a743b9671f9d294e15a6126e208317921b1c0bf
Daniel Bergey <bergey@alum.mit.edu>
2013-09-11 15:40:52 -0400

    all the tedious equation solving for MetaFont paths

--- src/Diagrams/TwoD/Path/Metafont/Internal.hs
+++ src/Diagrams/TwoD/Path/Metafont/Internal.hs
@@ -6,6 +6,7 @@ module Diagrams.TwoD.Path.Metafont.Internal where
 import Control.Lens hiding ((#), at)
 import Data.Maybe
 
+import Diagrams.CubicSpline.Internal
 import Diagrams.Prelude hiding ((&), view, over)
 
 import  Diagrams.TwoD.Path.Metafont.Types
@@ -123,6 +124,176 @@ controlPtDirs s = s
 fillDirs :: MFP -> MFP
 fillDirs p  = (copyDirsLoop . curlEnds) p & segs %~
               (copyDirsR . copyDirsL . map controlPtDirs)
+
+-- | Run all the rules required to fully specify all segment directions,
+-- but do not replace the Joins with ControlJoin.
+solve :: MFP -> MFPath Dir BasicJoin
+solve = solvePath . fillDirs
+
+-- | each sublist of groupSegments ss satisfies:
+-- isJust . d1 . pj . head
+-- isJust . d2 . pj . last
+-- all (isNothing . d1 . pj) . init . tail
+-- all (isNothing . d2 . pj) . init . tail
+-- That is, each sublist can be handled as a line,
+-- (except the first and last, if the initial MFP was a loop).
+groupSegments :: [MFS] -> [[MFS]]
+groupSegments [] = []
+groupSegments (s:ss) = (s:open):groupSegments rest where
+  (open,rest) = span (view $ pj.d1.to isNothing) ss
+
+-- | Calculate the tangent direction at all remaining points.
+-- This function dispatches all of the hard work to other functions.
+-- It distinguishes 3 cases:
+-- * A loop with no internal directions given.
+-- * A loop with one or more directions given.
+--     Mathematically, this is handled like a line, but the Loopness is
+--     preserved, so that the Diagrams Trail is a Loop.
+-- * A line, consisting of one or more segments as described in groupSegments.
+-- Note that the result type is different from the input, reflecting
+-- fully specified directions.
+solvePath :: MFP -> MFPath Dir BasicJoin
+solvePath (MFP False ss) = MFP False (concat . map solveLine . groupSegments $ ss)
+-- A simple loop.  All directions are unknown, curvature gives us enough equations.
+solvePath (MFP True ss) | all (view $ pj.d1.to isNothing) ss = MFP True $ solveLoop ss
+solvePath (MFP True ss) = MFP True ss'' where
+  ss' = groupSegments ss
+  ss'' = concat . map solveLine $ (init . tail $ ss') ++ [last ss' ++ head ss']
+
+-- | Calculate the tangent directions at all points.  The input list is assumed
+-- to form a loop; this is not checked.
+-- See setDirs for an explanation of offset angles.
+solveLoop :: [MFS] -> [MetafontSegment Dir BasicJoin]
+solveLoop ss = zipWith3 setDirs ss thetas phis where
+  segmentPairs = zip ss (tail . cycle $ ss)
+  thetas = loopDirs ss
+  phis = map negate $ zipWith (+) (map psi segmentPairs) (tail . cycle $ thetas)
+
+-- | Calculate the offset angles θ for the case of a loop.
+--   This is a system of (length ss) equations.  The first element of
+--   loopDirs ss is θ for the starting point of the first segment of ss.
+loopDirs :: [MFS] -> [Double]
+loopDirs ss = solveCyclicTriDiagonal lower diag upper products ll ur where
+  (lower, diag, upper, products, ll, ur) = loopEqs ss
+
+-- | Calculate the coefficients for the loop case, in the
+-- format required by solveCyclicTriDiagonal.
+-- See mf.web ¶ 273
+loopEqs ss = (lower, diag, upper, products, ll, ur) where
+  lower = map a (init ss)
+  sFirst = head ss
+  sLast = last ss
+  diag = zipWith (+) (map b $ [sLast] ++ ss) (map c ss)
+  upper = map d (init ss)
+  ur = a sLast
+  ll = d sLast
+  segmentPairs = zip ([last ss] ++ init ss) ss
+  products = zipWith (-)
+               [-1 * b l * psi s | s@(l,_) <- segmentPairs]
+               (zipWith (*)
+                (map d ss)
+                (map psi $ tail segmentPairs)
+                ++ [d sLast * psi (head segmentPairs)])
+
+-- | solveLine takes a list of segments where only the first and last points
+-- have known directions.  The type signature matches that of solveLoop, and the
+-- precondition is not checked.
+-- The equivalent MetaFont code (in make_choices) is written in terms of points,
+-- rather than segments.  See metafont code paragraphs 271--274.
+solveLine :: [MFS] -> [MetafontSegment Dir BasicJoin]
+solveLine [MFS z1 (PJ (Just (PathDirDir d1)) j (Just (PathDirDir d2))) z2] =
+  [MFS z1 (PJ d1 j d2) z2]
+solveLine ss = zipWith3 setDirs ss (init thetas) phis where
+  segmentPairs = zip (init ss) (tail ss)
+  thetas = lineDirs ss
+  phis :: [Double]
+  phis = map negate $ zipWith (+) (map psi segmentPairs ++ [0]) (tail thetas)
+
+-- | setDirs takes a segment with underspecified directions, and two offset
+-- angles, and sets the directions at both ends of the segment.
+-- The offset angle is measured between the direction vector at either end and
+-- the vector difference of the segment endpoints.
+setDirs :: MFS -- ^ The segment to be modified
+        -> Double -- ^ theta, the offset angle at the starting point
+        -> Double -- ^ phi, the ofset angle at the endpoint
+        -> MetafontSegment Dir BasicJoin
+setDirs (MFS z0 (PJ w0' j w1') z1) t p = MFS z0 (PJ w0 j w1) z1 where
+    offs  = z1 .-. z0
+    w0 = case w0' of
+      (Just (PathDirDir d)) -> d
+      _ -> offs # rotate (Turn t)
+    w1 = case w1' of
+      (Just (PathDirDir d)) -> d
+      _ -> offs # rotate (Turn (negate p))
+
+-- | psi (l,r) calculates the turning angle between segments l and r, if
+-- each segment were a straight line connecting its endpoints.  The endpoint of l
+-- is assumed to be the starting point of r; this is not checked.
+psi (l,r) = normalizeTurns t where
+  (Turn t) =  direction (mfSegmentOffset r) - direction (mfSegmentOffset l)
+
+-- | lineDirs calculates the offset angles θ for a Line.  Most of the work
+-- done by lineEqs and solveTriDiagonal, but lineDirs handles the separate cases
+-- of an empty list, and lists of length one.  See mf.web ¶ 280.
+lineDirs :: [MFS] -> [Double]
+lineDirs segs | length segs > 1 = solveTriDiagonal lower diag upper products where
+  (lower, diag, upper, products) = lineEqs segs
+lineDirs [] = []
+lineDirs [s] | leftCurl s && rightCurl s = [0, 1/2] where
+lineDirs [s] | rightCurl s = solveTriDiagonal [a] [1,c] [0] [normalizeTurns t, r] where
+  (a,c,r) = solveOneSeg s
+  (PathDirDir dir) = s^.pj.d1.to fromJust
+  (Turn t) = direction dir - direction (s^.x2 .-. s^.x1)
+lineDirs [s] | leftCurl s = reverse $ lineDirs [reverseSeg s]
+
+-- | Each intermediate point produces one curvature equation, as in loopEqs.
+-- The endpoint equations are the same as those for the single-segment line in
+-- lineDirs.
+-- lineEqs only works when segs has length > 1; this precondition is not checked.
+lineEqs :: [MFS] -> ([Double], [Double], [Double], [Double])
+lineEqs segs = (lower, diag, upper, products) where
+  segmentPairs = zip (init segs) (tail segs)
+  lower = map a (init segs) ++ [an]
+  diag  = c0 : zipWith (+) (map b (init segs)) (map c (tail segs)) ++ [cn]
+  upper = (d0 : map d (tail segs))
+  products = r0 : zipWith (-)
+               [-1 * b l * psi s | s@(l,r) <- segmentPairs]
+               (zipWith (*)
+                 (map d (tail $ segs))
+                 (map psi (tail segmentPairs)
+                ++ [0])) ++ [rn]
+  (d0,c0,_) = solveOneSeg . reverseSeg $ s0
+  r0 = r0' (s0^.pj.d1.to fromJust) where
+    r0' (PathDirDir d) = normalizeTurns t where
+                         (Turn t) = direction d - direction (s0^.x2 .-. s0^.x1)
+    r0' (PathDirCurl g) = negate $ d0 * psi (s0, segs!!1)
+  s0 = head segs
+  (an, cn, rn) = solveOneSeg (last segs)
+
+-- These functions calculate the coefficients in lineEqs, loopEqs
+-- They are derived in mf.web ¶ 272-273
+alpha, beta, a, b, c, d :: MFS -> Double
+alpha s = 1 / s^.pj.j.to fromLeft.t1.to getTension
+beta  s = 1 / s^.pj.j.to fromLeft.t2.to getTension
+a s = (alpha s) / (beta s **2 * mfSegmentLength s)
+b s = (3 - alpha s) / (beta s **2 * mfSegmentLength s)
+c s = (3 - beta s) / (alpha s **2 * mfSegmentLength s)
+d s = (beta s) / (alpha s **2 * mfSegmentLength s)
+
+-- | solveOneSeg calculates the coefficients for the final segment of a line,
+-- which may incidentally be the only segment.
+solveOneSeg :: MFS -> (Double, Double, Double)
+solveOneSeg s = (a, c, r) where
+  a = a' (s^.pj.d2.to fromJust) where
+    a' (PathDirDir _) = 0
+    a' (PathDirCurl g) = (3 - beta s) * (beta s) **2 * g / (alpha s **2) + alpha s
+  c = c' (s^.pj.d2.to fromJust) where
+       c' (PathDirDir _) = 1
+       c' (PathDirCurl g) = beta s **3 * g / (alpha s **2) + 3 - alpha s
+  r = r' (s^.pj.d2.to fromJust) where
+    r' (PathDirDir d) = normalizeTurns t where
+      (Turn t) = direction d - direction (s^.x2 .-. s^.x1)
+    r' (PathDirCurl _) = 0
 
 -- | Take a segment whose endpoint directions have been fully
 --   determined, and compute the control points to realize it as a

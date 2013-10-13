d2be50fe1eb1938a2e358523845819be61d3d51f
Daniel Bergey <bergey@alum.mit.edu>
2013-10-13 19:13:29 +0000

    Remove 'directions' metafont join
    'arriving' and 'leaving' can be combined with <>

--- src/Diagrams/TwoD/Path/Metafont/Combinators.hs
+++ src/Diagrams/TwoD/Path/Metafont/Combinators.hs
@@ -60,7 +60,3 @@ leaving d = mempty & d1.~ (Just . PathDirDir $ d)
 -- | A join with the right-end direction specified.
 arriving :: R2 -> Join
 arriving d = mempty & d2.~ (Just . PathDirDir $ d)
-
--- | A join with both directions specified.
-directions :: R2 -> R2 -> Join
-directions v1 v2 = leaving v1 & d2.~ (Just . PathDirDir $ v2)

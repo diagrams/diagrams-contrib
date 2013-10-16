f88d69581f82e5df712afb4bc57b6835745069f7
Daniel Bergey <bergey@alum.mit.edu>
2013-10-16 01:52:43 +0000

    Add examples to test bounding triangle / TensionAtLeast

--- tests/Diagrams/TwoD/Path/MetaFont/Tests.hs
+++ tests/Diagrams/TwoD/Path/MetaFont/Tests.hs
@@ -4,8 +4,8 @@
 
 import Data.Text (Text)
 import System.FilePath
-import Control.Arrow
-import Control.Lens hiding ((#), at)
+import Control.Arrow (second)
+import Control.Lens hiding ((#), at, from, to)
 
 import Diagrams.Prelude hiding (option)
 import Diagrams.Backend.SVG
@@ -14,17 +14,18 @@ import Diagrams.TwoD.Path.Metafont as MF
 import Diagrams.TwoD.Path.Metafont.Types
 
 litTests :: [(FilePath, Text)]
-litTests = [("p15a",  "(0,0)..(0,1)..(1,1)..(2,0)"),
-         ("p15b", "(1,0)..(0,0)..(0,1)..(2,1)..(2,0)..(1,0)"),
-         ("p15c",   "(1,0)..(0,0)..(0,1)..(2,1)..(2,0)..cycle"),
-         ("p15d",   "(1,0)..(0,0)..(0,1)..tension 1.2..(2,1)..(2,0)..cycle"),
-         ("p16a",      "(1,0)..(0,0)..tension 1 and 1.5..(0,1)..tension 1.5 and 1..(2,1)..(2,0)..cycle"),
-         ("p16b",   "(1,0)..(0,0){-1,0}..(0,1)..(2,1)..(2,0){-1,0}..cycle"),
-         ("p17a",      "(1,0)..(0,0){-1,-1}..(0,1)..(2,1)..(2,0){-1,1}..cycle"),
-         ("p17b", "(0,0){curl0}..(1,1){2,1}..{curl0}(2,1)"),
-         ("p17c", "(0,0){curl2}..(1,1){2,1}..{curl2}(2,1)"),
-         ("p19a", "(0,0)..controls (0,1) and (1,1)..(2,0)")
-         ]
+litTests = [
+    ("p15a", "(0,0)..(0,1)..(1,1)..(2,0)"),
+    ("p15b", "(1,0)..(0,0)..(0,1)..(2,1)..(2,0)..(1,0)"),
+    ("p15c", "(1,0)..(0,0)..(0,1)..(2,1)..(2,0)..cycle"),
+    ("p15d", "(1,0)..(0,0)..(0,1)..tension 1.2..(2,1)..(2,0)..cycle"),
+    ("p16a", "(1,0)..(0,0)..tension 1 and 1.5..(0,1)..tension 1.5 and 1..(2,1)..(2,0)..cycle"),
+    ("p16b", "(1,0)..(0,0){-1,0}..(0,1)..(2,1)..(2,0){-1,0}..cycle"),
+    ("p17a", "(1,0)..(0,0){-1,-1}..(0,1)..(2,1)..(2,0){-1,1}..cycle"),
+    ("p17b", "(0,0){curl0}..(1,1){2,1}..{curl0}(2,1)"),
+    ("p17c", "(0,0){curl2}..(1,1){2,1}..{curl2}(2,1)"),
+    ("p19a", "(0,0)..controls (0,1) and (1,1)..(2,0)")
+    ]
 
 astTests :: [(FilePath, Trail R2)]
 astTests = [("p125", let -- this is Knuth's branch4
@@ -47,7 +48,7 @@ combinatorTests = map (second metafont)
                   [ ("p15a_", z4.--.z1.--.z2.--.endpt z6)
                   , ("p15b_", z5.--.z4.--.z1.--.z3.--.z6.--.endpt z5)
                   , ("p15c_", z5.--.z4.--.z1.--.z3.--.z6.--.cyclePath)
-                  , ("p15d_", z5.--.z4.-tension 1.2-.z3.--.z6.--.cyclePath)
+                  , ("p15d_", z5.--.z4.--.z1.-tension 1.2-.z3.--.z6.--.cyclePath)
                   , ("p16a_", z5.--.z4.-tensions 1 1.5-.z1.-tensions 1.5 1-.z3.--.z6.--.cyclePath)
                   , ("p16b_", z5.--.z4.-goingLeft-.z1.--.z3.--.z6.-goingLeft-.cyclePath)
                   ] where
@@ -57,21 +58,32 @@ combinatorTests = map (second metafont)
   z4 = p2 (0,0)
   z5 = p2 (1,0)
   z6 = p2 (2,0)
-  goingLeft = leaving unitX
+  goingLeft = leaving unit_X
+
+boundedTests :: [(FilePath, Diagram SVG R2)]
+boundedTests = map (second (mconcat . map (illustrateTrailCtls . metafont)))
+               [ ("p18a", demo mempty)
+               , ("p18b", map (withJoin mempty . Deg) [0,10..90])
+               , ("p18c", demo bounded)
+               ] where
+  bounded = simpleJoin & j .~ (Just . Left $ TJ (TensionAtLeast 1) (TensionAtLeast 1))
+  left = leaving (fromDirection . Deg $ 60)
+  demo jj = map (withJoin jj . Deg) [0,(-10)..(-120)]
+  withJoin jj d = origin .- left <> jj <> arriving (fromDirection d) -. (endpt $ p2 (6,0))
 
 illustrateSegment :: FixedSegment R2 -> Diagram SVG R2
 illustrateSegment (FLinear from to) = position [
   (from, ptMark # fc blue),
   (to,   ptMark # fc blue)]
-illustrateSegment (FCubic from c1 c2 to) = position [
-  (c1, ptMark # fc red),
-  (c2, ptMark # fc red)] <> illustrateSegment (FLinear from to)
+illustrateSegment (FCubic from z1 z2 to) = position [
+  (z1, ptMark # fc red),
+  (z2, ptMark # fc red)] <> illustrateSegment (FLinear from to)
 
 ptMark :: Diagram SVG R2
 ptMark = circle 0.02 # lw 0
 
 illustrateTrailCtls :: Trail R2 -> Diagram SVG R2
-illustrateTrailCtls = mconcat . map illustrateSegment . fixTrail . flip at origin
+illustrateTrailCtls tr = strokeTrail tr <> (mconcat . map illustrateSegment . fixTrail . flip at origin $ tr)
 
 refPts :: Diagram SVG R2
 refPts = position $ zip (map p2 $ (,) <$> [0,1,2] <*> [0,1]) (repeat c) where
@@ -80,7 +92,7 @@ refPts = position $ zip (map p2 $ (,) <$> [0,1,2] <*> [0,1]) (repeat c) where
 toSVG :: (FilePath,Trail R2) -> IO ()
 toSVG (fn,tr) = do
   renderSVG (replaceExtension fn "svg") (Width 400) $
-    strokeTrail tr <> illustrateTrailCtls tr
+    illustrateTrailCtls tr
 
 renderMF :: (FilePath, Text) -> IO ()
 renderMF (fn, p) = do
@@ -90,5 +102,8 @@ renderMF (fn, p) = do
     Right tr -> toSVG (fn,tr)
 
 main :: IO ()
-main = mapM_ renderMF litTests >> mapM_ toSVG (astTests ++ combinatorTests)
+main = do
+    mapM_ renderMF litTests
+    mapM_ toSVG (astTests ++ combinatorTests)
+    mapM_ (\(fn, d) -> renderSVG (replaceExtension fn "svg") (Width 400) d) boundedTests
 

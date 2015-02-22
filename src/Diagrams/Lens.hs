0f678e7b8f0aacbd74a527721321b2700af9684e
Christopher Chalmers <c.chalmers@me.com>
2015-02-22 23:26:50 +0000

    Remove invalid lenses.
    They don't follow the lens laws and I can't see an easy way to fix them. I think it's best to just remove them.

--- src/Diagrams/Lens.hs
+++ src/Diagrams/Lens.hs
@@ -15,19 +15,13 @@
 -- License     :  BSD-style (see LICENSE)
 -- Maintainer  :  Michael Sloan <mgsloan at gmail>
 --
--- This module provides utilities for using "Control.Lens" with diagrams,
--- including orphan instances for the 'Wrapped' class.
+-- This module provides utilities for using "Control.Lens" with diagrams.
 module Diagrams.Lens
   (
-  -- * Diagrams.Align
-  _envelopeVMove
-  , _alignedVMove
   -- * Diagrams.BoundingBox
-  , _corners
-  , _boxExtents
+    _corners
   -- * Diagrams.Core.Types
   , _location
-  , _mkSubdiagram
   -- * Diagrams.Located
   , _Loc
   -- * Diagrams.Parametric
@@ -37,8 +31,6 @@ module Diagrams.Lens
   , _straight
   , _bezier3
   -- * Diagrams.Trail
-  , _lineVertices
-  , _lineOffsets
   , _lineSegments
   ) where
 
@@ -46,51 +38,7 @@ import           Control.Applicative
 import           Control.Lens
 import           Data.Foldable
 import           Diagrams.BoundingBox
-import           Diagrams.Core.Style
 import           Diagrams.Prelude
-
--- * Diagrams.Align
-
--- | A singleton 'Traversal' for an envelope vector, where modification of the
---   vector moves the origin appropriately.  No vector is traversed when the
---   envelope is empty.
---
---   This is the same as '_alignedVMove' with @1@ as the interpolation
---   parameter.
-_envelopeVMove
-  :: (Monoid a, HasOrigin a, Enveloped a, Num (N a))
-  => Vn a -> Traversal' a (Vn a)
-_envelopeVMove v f x = case envelopeVMay v x of
-  (Just p) -> (\p' -> moveOriginBy (p ^-^ p') x) <$> f p
-  Nothing -> pure x
-
--- | A singleton 'Traversal' for an alignment vector, where modification of the
---   vector moves the origin appropriately.  No vector is traversed when the
---   envelope is empty.
---
---   The interface mimics "Diagrams.Align.alignBy" in that the @d@ parameter
---   specifies an interpolation between two extremes of the envelope. @d = 1@
---   is on the envelope along the vector, whereas @d = -1@ is on the envelope,
---   away from the vector.
---
---   If you need a 'Point' instead of a vector, then compose with '_P'.
-_alignedVMove
-    :: (Monoid a, HasOrigin a, Enveloped a, Num (N a))
-  => Vn a -> N a -> Traversal' a (Vn a)
-_alignedVMove v d f x = case appEnvelope $ getEnvelope x of
-  (Just env) -> (\p' -> moveOriginBy (p ^-^ p') x) <$> f p
-    where
-      p = v ^* lerp' (env (negated v)) (env v) ((d + 1) / 2)
-      -- Constraints were pretty wacky otherwise.
-      lerp' l u t = (1 - t) * u + t * l
-  Nothing -> pure x
-
-{- TODO
-_traceMove :: Point v -> v -> Traversal (QDiagram b v n m) n
-_traceMove p v f x = case appTrace (trace x) p v of
-  Finite t ->
-  PosInfty -> pure x
--}
 
 
 -- * Diagrams.BoundingBox
@@ -103,17 +51,6 @@ _corners
 _corners f (getCorners -> Just (l, t)) = fromCorners <$> f l <*> f t
 _corners _ _ = pure emptyBox
 
--- | A lens that gets the extents of the box.  In order to change the extents,
---   this modifies the upper corner.
-_boxExtents
-  :: (Additive v, Foldable v, Num n, Ord n)
-  => Lens' (BoundingBox v n) (v n)
-_boxExtents = lens boxExtents setExtent
-  where
-    setExtent (getCorners -> Just (l, _)) x = fromCorners l (l .+^ x)
-    setExtent _ _ = emptyBox
-
-
 -- * Diagrams.Core.Types
 
 -- | Gets or set the 'location' of a 'Subdiagram'.
@@ -123,17 +60,10 @@ _location
 --TODO: Is this correct??
 _location = lens location (flip Diagrams.Prelude.moveTo)
 
-_mkSubdiagram
-  :: (HasLinearMap v, Metric v, OrderedField n, Semigroup m)
-  => Iso' (QDiagram b v n m) (Subdiagram b v n m)
-_mkSubdiagram = iso mkSubdiagram getSub
-
-
 -- * Diagrams.Located
 
 _Loc :: Iso (Located a) (Located a') (Point (V a) (N a), a) (Point (V a') (N a'), a')
 _Loc = iso viewLoc (uncurry $ flip Diagrams.Prelude.at)
-
 
 -- * Diagrams.Parametric
 
@@ -175,24 +105,7 @@ _bezier3 = prism' (\(c1, c2, c3) -> bezier3 c1 c2 c3) fromBezier3
     fromBezier3 (Cubic c1 c2 (OffsetClosed c3)) = Just (c1, c2, c3)
     fromBezier3 _ = Nothing
 
-
 -- * Diagrams.Trail
-
-_lineVertices
-  :: ( Metric v, OrderedField n
-     , Metric v', OrderedField n')
-  => Iso
-    (Located (Trail' Line v n)) (Located (Trail' Line v' n'))
-    [Point v n] [Point v' n']
-_lineVertices = iso lineVertices fromVertices
-
-_lineOffsets
-  :: ( Metric v, OrderedField n
-     , Metric v', OrderedField  n')
-  => Iso
-    (Trail' Line v n) (Trail' Line v' n')
-    [v n] [v' n']
-_lineOffsets = iso lineOffsets lineFromOffsets
 
 _lineSegments
   :: (Metric v', OrderedField n')

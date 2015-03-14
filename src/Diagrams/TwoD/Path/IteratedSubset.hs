8b90c8adddb339e0208d78608aac926018c85691
Daniel Bergey <bergey@alum.mit.edu>
2015-03-14 18:46:47 +0000

    remove extra Lens import
    most of `lens` is now exported from `Diagrams.Prelude`

--- src/Diagrams/TwoD/Path/IteratedSubset.hs
+++ src/Diagrams/TwoD/Path/IteratedSubset.hs
@@ -55,7 +55,6 @@ module Diagrams.TwoD.Path.IteratedSubset
 import           Diagrams.Core.Points       ()
 import           Diagrams.Prelude
 
-import           Control.Lens               ((^.))
 import           Control.Monad              (replicateM)
 import           Control.Monad.Random       (evalRandIO, getRandom, getRandomR)
 import           Control.Monad.Random.Class (MonadRandom)

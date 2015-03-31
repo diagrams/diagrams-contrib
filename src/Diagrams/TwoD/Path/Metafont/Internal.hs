diagrams/diagrams-contrib

commit e55638f8793a59d55aa2fc3e13fe1e59ea2b8b73

Remove AffineSpace instance for Direction R2
See diagrams-lib for explanation of how this instance breaks the laws.

src/Diagrams/TwoD/Path/Metafont/Internal.hs
@@ -248,7 +248,7 @@ setDirs (MFS z0 (PJ w0' jj w1') z1) t p = MFS z0 (PJ w0 jj w1) z1 where
 -- is assumed to be the starting point of r; this is not checked.
 psi :: (MetafontSegment p j1, MetafontSegment p j1) -> Double
 psi (l,r) = normalizeTurns t where
-  t = view turn $ direction (mfSegmentOffset r) .-. direction (mfSegmentOffset l)
+  t = view turn $ angleBetween (mfSegmentOffset r) (mfSegmentOffset l)
 
 -- | lineDirs calculates the offset angles θ for a Line.  Most of the work
 -- done by lineEqs and solveTriDiagonal, but lineDirs handles the separate cases
@@ -261,7 +261,7 @@ lineDirs [s] | leftCurl s && rightCurl s = [0, 0] where
 lineDirs [s] | rightCurl s = solveTriDiagonal [a] [1,c] [0] [normalizeTurns t, r] where
   (a,c,r) = solveOneSeg s
   (PathDirDir dir) = s^.pj.d1.to fromJust
-  t = view turn $ direction dir .-. direction (s^.x2 .-. s^.x1)
+  t = view turn $ angleBetween dir (s^.x2 .-. s^.x1)
 lineDirs [s] | leftCurl s = reverse $ lineDirs [reverseSeg s]
 lineDirs s = error $ "lineDirs was called on something inappropriate.  \
 \It should be called on a list of segments with directions specified at both ends.\
@@ -286,7 +286,7 @@ lineEqs ss = (lower, diag, upper, products) where
   (d0,c0,_) = solveOneSeg . reverseSeg $ s0
   r0 = r0' (s0^.pj.d1.to fromJust) where
     r0' (PathDirDir d) = normalizeTurns t where
-      t = view turn $ direction d .-. direction (s0^.x2 .-. s0^.x1)
+      t = view turn $ angleBetween d  (s0^.x2 .-. s0^.x1)
     r0' (PathDirCurl _) = negate $ d0 * psi (s0, ss!!1)
   s0 = head ss
   (an, cn, rn) = solveOneSeg (last ss)
@@ -314,7 +314,7 @@ solveOneSeg s = (a, c, r) where
        c' (PathDirCurl g) = beta s **3 * g / (alpha s **2) + 3 - alpha s
   r = r' (s^.pj.d2.to fromJust) where
     r' (PathDirDir d) = normalizeTurns t where
-      t = view turn $ direction d .-. direction (s^.x2 .-. s^.x1)
+      t = view turn $ angleBetween d  (s^.x2 .-. s^.x1)
     r' (PathDirCurl _) = 0
 
 -- | Take a segment whose endpoint directions have been fully
@@ -341,8 +341,8 @@ computeControls (MFS z0 (PJ w0 (Left (TJ a b)) w1) z1)
   where
     (u,v) = ctrlPts z0 w0 va vb w1 z1
     offs  = z1 .-. z0
-    theta = direction w0   .-. direction offs
-    phi   = direction offs .-. direction w1
+    theta = angleBetween w0 offs
+    phi   = angleBetween offs w1
     sinR  = sin . view rad
     boundingTriangleExists = signum (sinR theta) == signum (sinR phi)
                              && signum (sinR theta) == signum (sinR (theta^+^phi))
@@ -374,8 +374,8 @@ ctrlPts :: P2 -> R2 -> Double -> Double -> R2 -> P2 -> (P2, P2)
 ctrlPts z0 w0 va vb w1 z1 = (u,v)
   where
     offs  = z1 .-. z0
-    theta = direction w0   .-. direction offs
-    phi   = direction offs .-. direction w1
+    theta = angleBetween w0 offs
+    phi   = angleBetween offs w1
     u     = z0 .+^ (offs # rotate theta  # scale va)
     v     = z1 .-^ (offs # rotate (negateV phi) # scale vb)
 

src/Diagrams/TwoD/Path/IteratedSubset.hs
@@ -85,7 +85,7 @@ refineSegment t seg
     sOff = segOffset seg
     tOff = lineOffset t
     k    = magnitude sOff / magnitude tOff
-    r    = direction sOff .-. direction tOff
+    r    = angleBetween sOff tOff
 
 ------------------------------------------------------------
 -- Examples

src/Diagrams/TwoD/Sunburst.hs
@@ -79,7 +79,7 @@ toTree r s (c:cs) (Node _ ts) d a
       where
         n = length ts
         dt =  a ^/ (fromIntegral n)
-        qs = [d .+^ ((fromIntegral i) *^ dt ) | i <- [0..n]]
+        qs = [rotate ((fromIntegral i) *^ dt ) d  | i <- [0..n]]
         fs = toTree (r + s) s (cs ++ [c])
         ts' = zipWith3 fs ts (take (n-1) qs) (repeat dt)
 

commit c144137b28beeb0565c04d5f99605677bca7d883

Metafont equations require signed angle
`angleBetween`, introduced with the `Direction` type, is always positive.
The order of the arguments does not matter.

closes #48

This bug was introduced in e55638f8793a59d55aa2fc3e13fe1e59ea2b8b73
It looks as though I started to fix it in the next commit,
f573db1db3ac3a2aa60747b26e8bdf30a6d45272 but never finished.

`angleBetween` is still used in several parts of Metafont.Internal where
the result is immediately normalized to ± 1/2 turn.

src/Diagrams/TwoD/Path/Metafont/Internal.hs
@@ -250,7 +250,7 @@ setDirs (MFS z0 (PJ w0' jj w1') z1) t p = MFS z0 (PJ w0 jj w1) z1 where
 -- is assumed to be the starting point of r; this is not checked.
 psi :: RealFloat n => (MetafontSegment p j1 n, MetafontSegment p j1 n) -> n
 psi (l,r) = normalizeTurns t where
-  t = view turn $ (mfSegmentOffset r ^. _theta) ^-^ (mfSegmentOffset l ^. _theta)
+  t = view turn $ signedAngleBetween (mfSegmentOffset r) (mfSegmentOffset l)
 
 -- | lineDirs calculates the offset angles θ for a Line.  Most of the work
 -- done by lineEqs and solveTriDiagonal, but lineDirs handles the separate cases
@@ -334,7 +334,7 @@ solveOneSeg s = (a, c, r) where
 --   recovered by subtracting the control points from the endpoints
 --   anyway).
 computeControls
-  :: (Floating n, Ord n) => MetafontSegment (Dir n) (BasicJoin n) n
+  :: (RealFloat n, Ord n) => MetafontSegment (Dir n) (BasicJoin n) n
   -> MetafontSegment () (ControlJoin n) n
 computeControls (MFS z0 (PJ _ (Right cj) _) z1)
   = MFS z0 (PJ () cj ()) z1
@@ -345,8 +345,8 @@ computeControls (MFS z0 (PJ w0 (Left (TJ a b)) w1) z1)
     w1' = fromDirection w1
     (u,v) = ctrlPts z0 w0' va vb w1' z1
     offs  = z1 .-. z0
-    theta = angleBetween w0' offs
-    phi   = angleBetween offs w1'
+    theta = signedAngleBetween w0' offs
+    phi   = signedAngleBetween offs w1'
     sinR  = sin . view rad
     boundingTriangleExists = signum (sinR theta) == signum (sinR phi)
                              && signum (sinR theta) == signum (sinR (theta^+^phi))
@@ -374,12 +374,12 @@ computeControls (MFS z0 (PJ w0 (Left (TJ a b)) w1) z1)
 --   @z0 .. controls u and v .. z1@.
 --
 --   This uses a mysterious, magical formula due to John Hobby.
-ctrlPts :: (Floating n, Eq n) => P2 n -> V2 n -> n -> n -> V2 n -> P2 n -> (P2 n, P2 n)
+ctrlPts :: (RealFloat n, Eq n) => P2 n -> V2 n -> n -> n -> V2 n -> P2 n -> (P2 n, P2 n)
 ctrlPts z0 w0 va vb w1 z1 = (u,v)
   where
     offs  = z1 .-. z0
-    theta = angleBetween w0 offs
-    phi   = angleBetween offs w1
+    theta = signedAngleBetween w0 offs
+    phi   = signedAngleBetween offs w1
     u     = z0 .+^ (offs # rotate theta  # scale va)
     v     = z1 .-^ (offs # rotate (negated phi) # scale vb)
 
@@ -422,3 +422,6 @@ mfPathToSegments = fixCycleSegment . snd . mfPathToSegments'
             Just bj -> jj & j .~ bj
     fixCycleSegment (MFP True ss) = MFP True (ss & _last.x2 .~ ss^?!_head.x1)
     fixCycleSegment p = p
+
+signedAngleBetween :: RealFloat n => V2 n -> V2 n -> Angle n
+signedAngleBetween u v = (u ^. _theta) ^-^ (v ^. _theta)
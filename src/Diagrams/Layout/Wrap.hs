7812c686c7b43b61a22a9cdbd57f236a88d8a581
Christopher Chalmers <c.chalmers@me.com>
2018-10-13 10:31:34 +0100

    Uncomment Wrap and Follow modules

--- src/Diagrams/Layout/Wrap.hs
+++ src/Diagrams/Layout/Wrap.hs
@@ -15,109 +15,113 @@
 
 module Diagrams.Layout.Wrap where
 
---import           Control.Arrow    (first, (&&&))
---import qualified Data.Foldable    as F
---import           Data.List        (find, inits, tails)
---import           Diagrams.Prelude hiding (start)
---import           Linear.Epsilon
---import Geometry.Transform (translate)
+import           Control.Arrow    (first, (&&&))
+import qualified Data.Foldable    as F
+import           Data.List        (find, inits, tails)
+import           Diagrams.Prelude
+import           Linear.Epsilon
+import Geometry.Transform (translate)
 
 ---- TODO: Take into account the negative bounds, and iteratively refine
 ----   the list selection.
 
 ---- TODO: Search for a region before / after the target pick.
 
----- | @wrapDiagram@ post-processes the results of @wrapOutside@ /
-----   @wrapInside@ into a Diagram of the result.  This only works when
-----   applying them to a list of diagrams.
---wrapDiagram
---  :: Metric v
---  => ([(v Double, Diagram v)], [Diagram v]) -> Diagram v
---wrapDiagram = F.foldMap (uncurry translate) . fst
+-- | @wrapDiagram@ post-processes the results of @wrapOutside@ /
+--   @wrapInside@ into a Diagram of the result.  This only works when
+--   applying them to a list of diagrams.
+wrapDiagram
+  :: HasLinearMap v
+  => ([(v Double, Diagram v)], [Diagram v]) -> Diagram v
+wrapDiagram = F.foldMap (uncurry translate) . fst
 
----- | @wrapOutside@ is the same as @wrapInside@, but with an inverted
-----   predicate.
---wrapOutside :: ( Enveloped a, V a ~ v, N a ~ n
---               , Epsilon n -- See [6.12.3] note below
---               )
---            => (Point v n -> Bool) -> [v n] -> Point v n -> [a] -> ([(v n, a)], [a])
---wrapOutside f = wrapInside (not . f)
+-- | Given a vector, return a vector that extends to the edge of the
+--   envelope in the same direction.
+envV
+  :: (InSpace v n a, Enveloped a)
+  => v n -> a -> v n
+envV v a =
+  case extent v a of
+    Just (_,b) -> (b / norm v) *^ v
+    Nothing    -> zero
 
----- | @wrapInside@ greedily wraps content to fill a space defined by a
-----   predicate.  It is passed a list of vectors which express the
-----   order of dimensions to be filled.  In other words, wrapping RTL
-----   text is done by passing in [unitX, unitY], to first exhaust
-----   space horizontally, and then vertically.
-----
-----   Note that this function does not guarantee that there are not
-----   points inside each positioned item for which the predicate is
-----   False.  Instead, only the corners of the bounds, along each axii,
-----   are used.
---wrapInside :: forall a v n.
---           ( Enveloped a, V a ~ v, N a ~ n
---           , Metric v, OrderedField n, Epsilon n -- See [6.12.3] note below
---           )
---           => (Point v n -> Bool) -> [v n] -> Point v n
---           -> [a] -> ([(v n, a)], [a])
---wrapInside f axis start = rec zeros
--- where
---  zeros = map snd . zip axis $ repeat (0, 0)
---  norms = map normalize axis
---  getVector = sumV . zipWith (^*) norms
+-- | @wrapOutside@ is the same as @wrapInside@, but with an inverted
+--   predicate.
+wrapOutside :: ( Enveloped a, V a ~ v, N a ~ n
+               , Epsilon n -- See [6.12.3] note below
+               )
+            => (Point v n -> Bool) -> [v n] -> Point v n -> [a] -> ([(v n, a)], [a])
+wrapOutside f = wrapInside (not . f)
 
----- [[min bound, max bound]] of each axis.
---  boundsScalars :: a -> [[v n]]
---  boundsScalars d
---    = flip map norms
---    $ \v -> map (.-. origin) [envelopeP (negated v) d, envelopeP v d]
+-- | @wrapInside@ greedily wraps content to fill a space defined by a
+--   predicate.  It is passed a list of vectors which express the
+--   order of dimensions to be filled.  In other words, wrapping RTL
+--   text is done by passing in [unitX, unitY], to first exhaust
+--   space horizontally, and then vertically.
+--
+--   Note that this function does not guarantee that there are not
+--   points inside each positioned item for which the predicate is
+--   False.  Instead, only the corners of the bounds, along each axii,
+--   are used.
+wrapInside :: forall a v n.
+           ( Enveloped a, V a ~ v, N a ~ n
+           , Metric v, OrderedField n, Epsilon n -- See [6.12.3] note below
+           )
+           => (Point v n -> Bool) -> [v n] -> Point v n
+           -> [a] -> ([(v n, a)], [a])
+wrapInside f axis start = rec zeros
+ where
+  zeros = map snd . zip axis $ repeat (0, 0)
+  norms = map normalize axis
+  getVector = sumV . zipWith (^*) norms
 
----- Recurses on the list of items to lay out, maintaing a current set of
----- coefficients for the different axii, each paired with the maximum
----- boundary seen in that direction.
---  rec :: [(n, n)] -> [a] -> ([(v n, a)], [a])
---  rec _ [] = (mempty, [])
---  rec scs (d:ds)
----- Recurse a satisfactory position can be found, otherwise yields the
----- list of the remaining diagrams to be laid out.
---    = maybe (mempty, d:ds)
---            (\(v, scs') -> first ((v, d):) $ rec scs' ds)
---    $ find (check . fst) potential
---   where
---    curB = boundsScalars d
+-- [[min bound, max bound]] of each axis.
+  boundsScalars :: a -> [[v n]]
+  boundsScalars d
+    = flip map norms
+    $ \v -> [envV (negated v) d, envV v d]
 
----- Yields whether a given vector offset avoids collision.
---    check v = all (f . (start .+^) . sumV . (v:)) $ sequence curB
+-- Recurses on the list of items to lay out, maintaing a current set of
+-- coefficients for the different axii, each paired with the maximum
+-- boundary seen in that direction.
+  rec :: [(n, n)] -> [a] -> ([(v n, a)], [a])
+  rec _ [] = (mempty, [])
+  rec scs (d:ds)
+-- Recurse a satisfactory position can be found, otherwise yields the
+-- list of the remaining diagrams to be laid out.
+    = maybe (mempty, d:ds)
+            (\(v, scs') -> first ((v, d):) $ rec scs' ds)
+    $ find (check . fst) potential
+   where
+    curB = boundsScalars d
 
----- Updates the max bounds of an axis.
---    maxB [_, b] (x, m) = (x, max m $ x + norm b)
---    maxB _ _ = error "Diagrams.Layout.Wrap.wrapInside:maxB: pattern-match failure.  Please report this as a bug."
+-- Yields whether a given vector offset avoids collision.
+    check v = all (f . (start .+^) . sumV . (v:)) $ sequence curB
 
----- List of potential offsets to try, each paired with an updated list
----- of current / maxbound scalar coefficients for the axis.
---    potential = map (getVector . map fst &&& zipWith maxB curB)
----- Try setting an axis to its max-seen bound, zeroing all preceding.
---              . zipWith (++) (inits $ repeat (0, 0))
---              . map dupFirstY
---              . init $ tails scs
+-- Updates the max bounds of an axis.
+    maxB [_, b] (x, m) = (x, max m $ x + norm b)
+    maxB _ _ = error "Diagrams.Layout.Wrap.wrapInside:maxB: pattern-match failure.  Please report this as a bug."
 
---    dupFirstY ((_,x):xs) = (x,x):xs
---    dupFirstY _          = error "Diagrams.Layout.Wrap.wrapInside:dupFirstY: pattern-match failure.  Please report this as a bug."
+-- List of potential offsets to try, each paired with an updated list
+-- of current / maxbound scalar coefficients for the axis.
+    potential = map (getVector . map fst &&& zipWith maxB curB)
+-- Try setting an axis to its max-seen bound, zeroing all preceding.
+              . zipWith (++) (inits $ repeat (0, 0))
+              . map dupFirstY
+              . init $ tails scs
 
----- [6.12.3]: It should be possible to infer the Metric v and
-----   OrderedField n constraints from Enveloped a, v ~ V a,
-----   but GHC 6.12.3 cannot, so we redundantly list them here to
-----   preserve support for 6.12.3.
----- TODO this comment is obsolete; we certainly do not support GHC 6.12.3
+    dupFirstY ((_,x):xs) = (x,x):xs
+    dupFirstY _          = error "Diagrams.Layout.Wrap.wrapInside:dupFirstY: pattern-match failure.  Please report this as a bug."
 
-----   Attempt at diagrams-haddock example, but I don't understand how Wrap works
-----
-----   > import Diagrams.Layout.Wrap
-----   > import Control.Arrow (first)
-----   > wrapInsideEx = position ((map . first $ (origin .+^)) ds)
-----   >   where (ds,_) = wrapInside
-----   >                    (getAny . (runQuery . query $ (circle 15 :: D V2 n)))
-----   >                    [unitX, unitY]
-----   >                    (origin)
-----   >                    (repeat (circle 1 # fc black))
-----
-----   <diagrams/wrapInsideEx.svg#diagram=wrapInsideEx&width=200>
+--   Attempt at diagrams-haddock example, but I don't understand how Wrap works
+--
+--   > import Diagrams.Layout.Wrap
+--   > import Control.Arrow (first)
+--   > wrapInsideEx = position ((map . first $ (origin .+^)) ds)
+--   >   where (ds,_) = wrapInside
+--   >                    (getAny . (runQuery . query $ (circle 15 :: D V2 n)))
+--   >                    [unitX, unitY]
+--   >                    (origin)
+--   >                    (repeat (circle 1 # fc black))
+--
+--   <diagrams/wrapInsideEx.svg#diagram=wrapInsideEx&width=200>
--- src/Diagrams/TwoD/Path/Follow.hs
+++ src/Diagrams/TwoD/Path/Follow.hs
@@ -15,74 +15,74 @@
 --
 -----------------------------------------------------------------------------
 
-module Diagrams.TwoD.Path.Follow where
-    -- ( Following, follow, unfollow
-    -- ) where
+module Diagrams.TwoD.Path.Follow
+    ( Following, follow, unfollow
+    ) where
 
--- import           Diagrams.Prelude
+import           Diagrams.Prelude
 
--- import           Data.Monoid.SemiDirectProduct.Strict
+import           Data.Monoid.SemiDirectProduct.Strict
 
--- -- | @Following@ is just like @Trail' Line V2@, except that it has a
--- --   different 'Monoid' instance.  @Following@ values are
--- --   concatenated, just like regular lines, except that they are also
--- --   rotated so the tangents match at the join point.  In addition,
--- --   they are normalized so the tangent at the start point is in the
--- --   direction of the positive x axis (essentially we are considering
--- --   trails equivalent up to rotation).
--- --
--- --   Pro tip: you can concatenate a list of trails so their tangents
--- --   match using 'ala' from "Control.Lens", like so:
--- --
--- --     @ala follow foldMap :: [Trail' Line V2 n] -> Trail' Line V2 n@
--- --
--- --   This is illustrated in the example below.
--- --
--- --   <<#dia=followExample&width=400>>
--- --
--- --   > import Control.Lens (ala)
--- --   >
--- --   > wibble :: Trail' Line V2 Double
--- --   > wibble = hrule 1 <> hrule 0.5 # rotateBy (1/6) <> hrule 0.5 # rotateBy (-1/6) <> a
--- --   >   where a = arc (xDir # rotateBy (-1/4)) (1/5 @@ turn)
--- --   >           # scale 0.7
--- --   >
--- --   > followExample =
--- --   >   [ wibble
--- --   >   , wibble
--- --   >     # replicate 5
--- --   >     # ala follow foldMap
--- --   >   ]
--- --   >   # map stroke
--- --   >   # map centerXY
--- --   >   # vsep 1
--- --   >   # frame 0.5
--- --
--- newtype Following n
-  -- = Following { unFollowing :: Semi (Trail' Line V2 n) (Angle n) }
-  -- deriving (Monoid)
+-- | @Following@ is just like @Line V2@, except that it has a
+--   different 'Monoid' instance.  @Following@ values are
+--   concatenated, just like regular lines, except that they are also
+--   rotated so the tangents match at the join point.  In addition,
+--   they are normalized so the tangent at the start point is in the
+--   direction of the positive x axis (essentially we are considering
+--   trails equivalent up to rotation).
+--
+--   Pro tip: you can concatenate a list of trails so their tangents
+--   match using 'ala' from "Control.Lens", like so:
+--
+--     @ala follow foldMap :: [Trail' Line V2 n] -> Trail' Line V2 n@
+--
+--   This is illustrated in the example below.
+--
+--   <<#dia=followExample&width=400>>
+--
+--   > import Control.Lens (ala)
+--   >
+--   > wibble :: Trail' Line V2 Double
+--   > wibble = hrule 1 <> hrule 0.5 # rotateBy (1/6) <> hrule 0.5 # rotateBy (-1/6) <> a
+--   >   where a = arc (xDir # rotateBy (-1/4)) (1/5 @@ turn)
+--   >           # scale 0.7
+--   >
+--   > followExample =
+--   >   [ wibble
+--   >   , wibble
+--   >     # replicate 5
+--   >     # ala follow foldMap
+--   >   ]
+--   >   # map stroke
+--   >   # map centerXY
+--   >   # vsep 1
+--   >   # frame 0.5
+--
+newtype Following n
+  = Following { unFollowing :: Semi (Line V2 n) (Angle n) }
+  deriving (Monoid)
 
--- -- | Note this is only an iso when considering trails equivalent up to
--- --   rotation.
--- instance RealFloat n => Wrapped (Following n) where
-  -- type Unwrapped (Following n) = Trail' Line V2 n
+-- | Note this is only an iso when considering trails equivalent up to
+--   rotation.
+instance RealFloat n => Wrapped (Following n) where
+  type Unwrapped (Following n) = Line V2 n
 
-  -- _Wrapped' = iso unfollow follow
+  _Wrapped' = iso unfollow follow
 
--- instance RealFloat n => Rewrapped (Following n) (Following n')
+instance RealFloat n => Rewrapped (Following n) (Following n')
 
--- -- | Create a @Following@ from a line, normalizing it (by rotation)
--- --   so that it starts in the positive x direction.
--- follow :: RealFloat n => Trail' Line V2 n -> Following n
--- follow t = Following $ (t # rotate (signedAngleBetween unitX s)) `tag` theta
-  -- where
-    -- s     = tangentAtStart t
-    -- e     = tangentAtEnd t
-    -- theta = signedAngleBetween e s
+-- | Create a @Following@ from a line, normalizing it (by rotation)
+--   so that it starts in the positive x direction.
+follow :: RealFloat n => Line V2 n -> Following n
+follow t = Following $ (t # rotate (signedAngleBetween unitX s)) `tag` theta
+  where
+    s     = tangentAtStart t
+    e     = tangentAtEnd t
+    theta = signedAngleBetween e s
 
--- -- | Project out the line from a `Following`.
--- --
--- --   If trails are considered equivalent up to rotation, then
--- --   'unfollow' and 'follow' are inverse.
--- unfollow :: Following n -> Trail' Line V2 n
--- unfollow = untag . unFollowing
+-- | Project out the line from a `Following`.
+--
+--   If trails are considered equivalent up to rotation, then
+--   'unfollow' and 'follow' are inverse.
+unfollow :: Following n -> Line V2 n
+unfollow = untag . unFollowing
